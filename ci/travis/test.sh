c55732392ca858fbdcb01a5011082683543baecb
Brendan Knapp <brendan.knapp@nps.edu>
2020-02-14 10:06:48 -0800

    travis

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -ex
 
-# run `cargo test` only if testing against cpython.
-if ! [[ $FEATURES == *"pypy"* ]]; then
-  cargo test --features "$FEATURES num-bigint num-complex"
-  ( cd pyo3-derive-backend; cargo test )
-else
-  # check that pypy at least builds
-  PYTHON_SYS_EXECUTABLE="/opt/anaconda/envs/pypy3/bin/pypy3" cargo build;
-fi
-
 if [ "$TRAVIS_JOB_NAME" = "Minimum nightly" ]; then
     pip install --pre black==19.3b0
     make lint
